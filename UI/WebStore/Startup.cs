66ab91d10119942325ea38d5a5f107e80889c5fc
Шмачилин Павел Александрович <shmachilin@gmail.com>
2019-09-20 21:00:39 +0300

    Клиент+контроллер для сервиса товаров WebAPI

--- UI/WebStore/Startup.cs
+++ UI/WebStore/Startup.cs
@@ -7,6 +7,7 @@ using Microsoft.EntityFrameworkCore;
 using Microsoft.Extensions.Configuration;
 using Microsoft.Extensions.DependencyInjection;
 using WebStore.Clients.Employees;
+using WebStore.Clients.Products;
 using WebStore.Clients.Values;
 using WebStore.DAL.Context;
 using WebStore.Data;
@@ -36,7 +37,8 @@ namespace WebStore
             services.AddSingleton<IEmployeesData, EmployeesClient>();
             //services.AddSingleton<IEmployeesData, InMemoryEmployeesData>();
             //services.AddSingleton<IProductData, InMemoryProductData>();
-            services.AddScoped<IProductData, SqlProductData>();
+            services.AddScoped<IProductData, ProductsClient>();
+            //services.AddScoped<IProductData, SqlProductData>();
             services.AddScoped<ICartService, CookieCartService>();
             services.AddScoped<IOrderService, SqlOrdersService>();
 
