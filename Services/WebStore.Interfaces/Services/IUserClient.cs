004923cd5e2349028b702d42f80f5516b2f88f3b
Шмачилин Павел Александрович <shmachilin@gmail.com>
2019-09-23 19:22:37 +0300

    DTO-объекты cbcntvs Identity

---
+++ Services/WebStore.Interfaces/Services/IUserClient.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+using Microsoft.AspNetCore.Identity;
+using WebStore.Domain.Entities;
+
+namespace WebStore.Interfaces.Services
+{
+    public interface IUserClient : IUserRoleStore<User>,
+                                   IUserPasswordStore<User>,
+                                   IUserEmailStore<User>,
+                                   IUserPhoneNumberStore<User>,
+                                   IUserClaimStore<User>,
+                                   IUserTwoFactorStore<User>,
+                                   IUserLoginStore<User>,
+                                   IUserLockoutStore<User>
+    {
+    }
+}
