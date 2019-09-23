be0e3b6aa2f336ce9a3350dc6219da79587ba4fa
Шмачилин Павел Александрович <shmachilin@gmail.com>
2019-09-23 18:39:44 +0300

    Рефакторинг базового клиента

--- Services/WebStore.Clients/Base/BaseClient.cs
+++ Services/WebStore.Clients/Base/BaseClient.cs
@@ -18,10 +18,7 @@ namespace WebStore.Clients.Base
         {
             _ServiceAddress = ServiceAddress;
 
-            _Client = new HttpClient
-            {
-                BaseAddress = new Uri(configuration["ClientAddress"])
-            };
+            _Client = new HttpClient { BaseAddress = new Uri(configuration["ClientAddress"]) };
             var headers = _Client.DefaultRequestHeaders.Accept;
 
             headers.Clear();
@@ -29,29 +26,20 @@ namespace WebStore.Clients.Base
             headers.Add(new MediaTypeWithQualityHeaderValue("application/json"));
         }
 
-        protected async Task<T> GetAsync<T>(string url) where T : new()
-        {
-            var response = await _Client.GetAsync(url);
-            if (response.IsSuccessStatusCode)
-                return await response.Content.ReadAsAsync<T>();
-            return new T();
-        }
+        protected async Task<T> GetAsync<T>(string url) where T : new() => 
+            (await _Client.GetAsync(url)).IsSuccessStatusCode 
+                ? await (await _Client.GetAsync(url)).Content.ReadAsAsync<T>() 
+                : new T();
 
         protected T Get<T>(string url) where T : new() => GetAsync<T>(url).Result;
 
-        protected async Task<HttpResponseMessage> PostAsync<T>(string url, T item)
-        {
-            var response = await _Client.PostAsJsonAsync(url, item);
-            return response.EnsureSuccessStatusCode();
-        }
+        protected async Task<HttpResponseMessage> PostAsync<T>(string url, T item) => 
+            (await _Client.PostAsJsonAsync(url, item)).EnsureSuccessStatusCode();
 
         protected HttpResponseMessage Post<T>(string url, T item) => PostAsync(url, item).Result;
 
-        protected async Task<HttpResponseMessage> PutAsync<T>(string url, T item)
-        {
-            var response = await _Client.PutAsJsonAsync(url, item);
-            return response.EnsureSuccessStatusCode();
-        }
+        protected async Task<HttpResponseMessage> PutAsync<T>(string url, T item) => 
+            (await _Client.PutAsJsonAsync(url, item)).EnsureSuccessStatusCode();
 
         protected HttpResponseMessage Put<T>(string url, T item) => PutAsync(url, item).Result;
 
