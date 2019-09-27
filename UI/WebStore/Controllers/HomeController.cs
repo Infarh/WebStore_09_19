25b48bd0349f3e5693732810bb824c00f88288ff
Шмачилин Павел Александрович <shmachilin@gmail.com>
2019-09-27 19:44:09 +0300

    Добавление логирования в контроллеры

--- UI/WebStore/Controllers/HomeController.cs
+++ UI/WebStore/Controllers/HomeController.cs
@@ -1,9 +1,14 @@
 ﻿using Microsoft.AspNetCore.Mvc;
+using Microsoft.Extensions.Logging;
 
 namespace WebStore.Controllers
 {
     public class HomeController : Controller
     {
+        private readonly ILogger<HomeController> _Logger;
+
+        public HomeController(ILogger<HomeController> Logger) { _Logger = Logger; }
+
         //[ActionFilterAsync]
         public IActionResult Index() => View();
 
@@ -11,10 +16,18 @@ namespace WebStore.Controllers
 
         public IActionResult Checkout() => View();
 
-        public IActionResult Blog() => View();
+        public IActionResult Blog()
+        {
+            _Logger.LogInformation("Запрос блога");
+            return View();
+        }
 
         public IActionResult BlogSingle() => View();
 
-        public IActionResult Error404() => View();
+        public IActionResult Error404()
+        {
+            _Logger.LogWarning("Запрос страницы 404");
+            return View();
+        }
     }
 }
